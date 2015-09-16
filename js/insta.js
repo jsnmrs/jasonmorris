b010f05c7090e0f2b645c1b245b1291d157e5866
Jason Morris <jasonmorris@gmail.com>
2015-09-15 20:38:06 -0400

    crunch JS a bit

--- js/insta.js
+++ js/insta.js
@@ -1,17 +1,1 @@
-$(document).ready(function () {
-    if ($(".photos").length > 0) {
-        $('.photos').html('<img src="/img/loading.gif" class="loading" alt="loading" />');
-        $.ajax({
-            type: "GET",
-            dataType: "jsonp",
-            cache: true,
-            url: "https://api.instagram.com/v1/users/self/media/recent/?access_token=8067894.64f3363.b86b57e0933b45fe811a9c9aaab38f66",
-            success: function (e) {
-              $('.photos').html('');
-              for (var t = 0; t < 8; t++) {
-                $(".photos").append("<article><a href='" + e.data[t].link + "'><img class='instagram-image' src='" + e.data[t].images.low_resolution.url + "' /><div class='post-title'><h2>" + e.data[t].caption.text + "</h2></div></a></article>");
-              }
-            }
-        });
-    }
-});
+$(document).ready(function(){if($(".photos").length>0){$('.photos').html('<img src="/img/loading.gif" class="loading" alt="loading" />');$.ajax({type:"GET",dataType:"jsonp",cache:true,url:"https://api.instagram.com/v1/users/self/media/recent/?access_token=8067894.64f3363.b86b57e0933b45fe811a9c9aaab38f66",success:function(e){$('.photos').html('');for(var t=0;t<8;t++){$(".photos").append("<article><a href='"+e.data[t].link+"'><img class='instagram-image' src='"+e.data[t].images.low_resolution.url+"' /><div class='post-title'><h2>"+e.data[t].caption.text+"</h2></div></a></article>")}}})}});
