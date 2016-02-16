c067690c845946e1fee3ef924f4d8c92ea3a40dd
Jason Morris <jasonmorris@gmail.com>
2016-02-16 02:51:32 -0500

    Overhaul grunt tasks, update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,12 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    // JSHint - Error check javascript
     jshint: {
-      beforeconcat: ['_test/jasonmorris.test.js']
+      beforeconcat: ['Gruntfile.js', '_test/jasonmorris.test.js']
     },
 
+    // Uglify - Minify js files
     uglify: {
       html5shiv: {
         files: {
@@ -19,26 +21,20 @@ module.exports = function(grunt) {
       }
     },
 
-    watch: {
-      options: {
-        livereload: true,
-        spawn: false
-      },
-      js: {
-        files: ['js/**/*.js'],
-        tasks: ['jshint', 'uglify']
-      }
-    },
-
+    // Shell - Jekyll build and serve tasks via shell
     shell: {
-      build: {
+      jekyllBuild: {
         command: 'bundle exec jekyll build'
       },
-      linkcheck: {
+      jekyllServe: {
+        command: 'bundle exec jekyll serve --watch'
+      },
+      linkCheck: {
         command: "htmlproof ./_site --alt-ignore '/.*/'"
       }
     },
 
+    // HTML lint - run html validation on compiled site
     htmllint: {
       all: {
         options: {
@@ -49,6 +45,7 @@ module.exports = function(grunt) {
       }
     },
 
+    // SCSS lint - check SCSS formatting
     scsslint: {
       all: [
         '_sass/*.scss',
@@ -60,6 +57,7 @@ module.exports = function(grunt) {
       }
     },
 
+    // Accessibility - run accessibility scan on compiled site
     accessibility: {
       options : {
         accessibilityLevel: 'WCAG2AA',
@@ -69,10 +67,8 @@ module.exports = function(grunt) {
           error: true
         },
         ignore : [
-          'WCAG2A.Principle1.Guideline1_1.1_1_1.H67.2', // empty alt tag warning
           'WCAG2AA.Principle1.Guideline1_1.1_1_1.H67.2', // empty alt tag warning
-          'WCAG2AA.Principle1.Guideline1_3.1_3_1.H48', // navigation section warning
-          'WCAG2AA.Principle1.Guideline1_4.1_4_3.G18.Abs' // abs pos contrast warning
+          'WCAG2AA.Principle1.Guideline1_3.1_3_1.H48' // navigation section warning
           ]
       },
       test : {
@@ -80,6 +76,7 @@ module.exports = function(grunt) {
       }
     },
 
+    // Update - find new versions of Grunt dependencies
     devUpdate: {
       main: {
         options: {
@@ -100,12 +97,31 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
   require('time-grunt')(grunt);
 
-  grunt.registerTask('default', ['jshint', 'uglify']);
-  grunt.registerTask('work', ['default', 'watch']);
-  grunt.registerTask('audit-html', ['htmllint']);
-  grunt.registerTask('audit-scss', ['scsslint']);
-  grunt.registerTask('audit-a11y', ['accessibility']);
-  grunt.registerTask('build-test', ['shell:build', 'htmllint', 'accessibility', 'scsslint', 'shell:linkcheck']);
+  // grunt - runs js, then serve tasks (see below)
+  grunt.registerTask('default', ['js', 'serve']);
+
+  // grunt serve - Serve and watch Jekyll site + SCSS compilation
+  grunt.registerTask('serve', ['shell:jekyllServe']);
+
+  // grunt build - Run js, imagemin tasks, then build Jekyll site + SCSS compilation
+  grunt.registerTask('build', ['js', 'shell:jekyllBuild']);
+
+  // grunt js - Error checks, concatenation, minify JS
+  grunt.registerTask('js', ['jshint', 'uglify']);
+
+  // grunt validate-html - Validate compiled site's HTML
+  grunt.registerTask('validate-html', ['htmllint']);
+
+  // grunt validate-scss - Validate compiled site's SCSS
+  grunt.registerTask('validate-scss', ['scsslint']);
+
+  // grunt a11y - Validate compiled site's accessibility
+  grunt.registerTask('a11y', ['accessibility']);
+
+  // grunt build-test - Task for TravicCI to run
+  grunt.registerTask('build-test', ['shell:jekyllBuild', 'htmllint', 'accessibility', 'scsslint', 'shell:linkCheck']);
+
+  // grunt update - Find new versions of Grunt libraries
   grunt.registerTask('update', ['devUpdate']);
 
 };
