597627305841e88bb32bebc806eb1978154fd3fb
Jason Morris <jasonmorris@gmail.com>
2020-05-02 11:00:07 +0000

    Housekeeping (#169)
    * Clean up deps, add prettier to build
    
    * prettier formatting
    
    * Add prettier SCSS step
    
    * prettier formatting
    
    * Split actions

--- .eleventy.js
+++ .eleventy.js
@@ -1,39 +1,39 @@
 const cleanCSS = require("clean-css");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
-const htmlMinTransform = require('./_transforms/html-min.js');
+const htmlMinTransform = require("./_transforms/html-min.js");
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addLayoutAlias('home', 'layouts/home.html');
-  eleventyConfig.addLayoutAlias('page', 'layouts/page.html');
-  eleventyConfig.addLayoutAlias('post', 'layouts/post.html');
-  eleventyConfig.addLayoutAlias('compress', 'layouts/compress.html');
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addLayoutAlias("home", "layouts/home.html");
+  eleventyConfig.addLayoutAlias("page", "layouts/page.html");
+  eleventyConfig.addLayoutAlias("post", "layouts/post.html");
+  eleventyConfig.addLayoutAlias("compress", "layouts/compress.html");
 
-  eleventyConfig.addPassthroughCopy('fonts');
-  eleventyConfig.addPassthroughCopy('img');
-  eleventyConfig.addPassthroughCopy('sw.js');
-  eleventyConfig.addPassthroughCopy('favicon.ico');
-  eleventyConfig.addPassthroughCopy('manifest.json');
-  eleventyConfig.addPassthroughCopy('apple-touch-icon.png');
-  eleventyConfig.addPassthroughCopy('.htaccess');
+  eleventyConfig.addPassthroughCopy("fonts");
+  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("sw.js");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
+  eleventyConfig.addPassthroughCopy("manifest.json");
+  eleventyConfig.addPassthroughCopy("apple-touch-icon.png");
+  eleventyConfig.addPassthroughCopy(".htaccess");
 
   eleventyConfig.setLiquidOptions({
     dynamicPartials: true,
     strict_filters: true,
-    root: ["_includes"]
+    root: ["_includes"],
   });
 
-  eleventyConfig.addFilter("cssmin", function(code) {
+  eleventyConfig.addFilter("cssmin", function (code) {
     return new cleanCSS({}).minify(code).styles;
   });
 
   eleventyConfig.addPlugin(pluginRss);
 
-  eleventyConfig.addTransform('htmlmin', htmlMinTransform);
+  eleventyConfig.addTransform("htmlmin", htmlMinTransform);
 
   return {
     dir: {
       input: "./",
-      output: "./_site"
-    }
+      output: "./_site",
+    },
   };
 };
