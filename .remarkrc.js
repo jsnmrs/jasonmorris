021b8f74cd3af840b8cf2d19e4adcd1305f7e055
Katy DeCorah <katydecorah@gmail.com>
2020-06-22 15:21:28 -0400

    Add remark linters (#204)
    * Add remark-lint-alt-text and remark-frontmatter-validator
    
    * update remark-frontmatter-validator, use `type: "date"`, fix config

---
+++ .remarkrc.js
@@ -0,0 +1,41 @@
+exports.plugins = [
+  "@double-great/remark-lint-alt-text",
+  ["remark-frontmatter", ["yaml"]],
+  [
+    "remark-frontmatter-validator",
+    [
+      2,
+      {
+        category: {
+          oneOf: ["code", "bikes", "photo", "diy"],
+          type: "string",
+        },
+        date: {
+          required: true,
+          type: "date",
+        },
+        layout: {
+          match: "post",
+          required: true,
+          type: "string",
+        },
+        permalink: {
+          required: true,
+          type: "string",
+        },
+        tags: {
+          match: "post",
+          required: true,
+          type: "string",
+        },
+        title: {
+          required: true,
+          type: "string",
+        },
+        offline: {
+          type: "boolean",
+        },
+      },
+    ],
+  ],
+];
