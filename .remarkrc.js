bc113f952efc11b2f81f7d7d11f33bd1e4558f93
Katy DeCorah <katydecorah@gmail.com>
2020-06-22 13:16:35 -0400

    Add remark-lint-alt-text and remark-frontmatter-validator

---
+++ .remarkrc.js
@@ -0,0 +1,43 @@
+exports.plugins = [
+  [
+    "@double-great/remark-lint-alt-text",
+    ["remark-frontmatter", ["yaml"]],
+    [
+      "remark-frontmatter-validator",
+      [
+        2,
+        {
+          category: {
+            oneOf: ["code", "bikes", "photo", "diy"],
+            type: "string",
+          },
+          date: {
+            required: true,
+            match: "^\\d\\d\\d\\d-\\d\\d-\\d\\dT\\d\\d:\\d\\d:\\d\\d",
+          },
+          layout: {
+            match: "post",
+            required: true,
+            type: "string",
+          },
+          permalink: {
+            required: true,
+            type: "string",
+          },
+          tags: {
+            match: "post",
+            required: true,
+            type: "string",
+          },
+          title: {
+            required: true,
+            type: "string",
+          },
+          offline: {
+            type: "boolean",
+          },
+        },
+      ],
+    ],
+  ],
+];
