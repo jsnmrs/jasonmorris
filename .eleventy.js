b16eac477d9de13766078359fab373b304352de5
Jason Morris <jasonmorris@gmail.com>
2021-02-26 06:20:29 -0500

    manifest pass through

--- .eleventy.js
+++ .eleventy.js
@@ -12,7 +12,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("sw.js");
   eleventyConfig.addPassthroughCopy("favicon.ico");
-  eleventyConfig.addPassthroughCopy("manifest.json");
+  eleventyConfig.addPassthroughCopy("manifest.webmanifest");
   eleventyConfig.addPassthroughCopy("apple-touch-icon.png");
   eleventyConfig.addPassthroughCopy(".htaccess");
 
