cb9136bfb58bb90dbd8a7915a01d00980a629227
Jason Morris <jasonmorris@gmail.com>
2021-12-15 22:17:22 -0500

    More rinse (#439)
    * Add htmlhint to the mix
    
    * unnecessary exception
    
    * tighten the JS rules
    
    * Safely adding to DOM

--- js/facade.js
+++ js/facade.js
@@ -14,20 +14,27 @@
   }
 
   function videoClick(event) {
-    var videoHolder = this.parentNode.children[1];
+    var videoHolder = this.parentNode.children[1],
+      videoIframe;
 
     event.preventDefault();
     videoHolder.classList.add("video");
-    videoHolder.innerHTML =
-      '<iframe src="https://player.vimeo.com/video/' +
-      videoHolder.dataset.id +
-      '?dnt=true&amp;title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" title="' +
-      videoHolder.dataset.title +
-      '" width="' +
-      videoHolder.dataset.width +
-      '" height="' +
-      videoHolder.dataset.height +
-      '" frameborder="0" allowfullscreen></iframe>';
+    videoIframe = document.createElement("iframe");
+    videoIframe.setAttribute(
+      "src",
+      "https://player.vimeo.com/video/" +
+        videoHolder.dataset.id +
+        "?dnt=true&amp;title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff)"
+    );
+    videoIframe.setAttribute(
+      "title",
+      videoHolder.dataset.title + " — embedded video"
+    );
+    videoIframe.setAttribute("width", videoHolder.dataset.width);
+    videoIframe.setAttribute("height", videoHolder.dataset.height);
+    videoIframe.setAttribute("frameborder", "0");
+    videoIframe.setAttribute("allowfullscreen", "");
+    videoHolder.appendChild(videoIframe);
     this.remove();
   }
 
