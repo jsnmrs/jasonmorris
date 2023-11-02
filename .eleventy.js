62a43e19e0c9a8a2826b8940a1f621c44169bfc9
Jason Morris <jasonmorris@fastmail.com>
2023-11-02 19:03:50 -0400

    Conditional figure wrapper, too

--- .eleventy.js
+++ .eleventy.js
@@ -133,9 +133,11 @@ module.exports = function (eleventyConfig) {
         });
       })();
 
-      return `<figure><picture>${sourcesAvif}${sourcesWebp}${sourcesVintage}<img src="${fullPath}-240.${ext}" alt="${alt}" loading="lazy" width="${width}" height="${height}"></picture>${
+      return `${
+        caption ? `<figure>` : ""
+      }<picture>${sourcesAvif}${sourcesWebp}${sourcesVintage}<img src="${fullPath}-240.${ext}" alt="${alt}" loading="lazy" width="${width}" height="${height}"></picture>${
         caption ? `<figcaption>${caption}</figcaption>` : ""
-      }</figure>`;
+      }${caption ? `</figure>` : ""}`;
     },
   );
   // Usage: {% picture "file-name", "jpg", "240", "159", "1600" "Alt text.", "Caption" %}
